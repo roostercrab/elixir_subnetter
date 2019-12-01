56078cc8a9fbc277833851091934f9d783c17269
roostercrab <jesse.shea@qixingdesign.com>
2019-12-01 14:08:08 -0800

    stuff

--- lib/subnetter.ex
+++ lib/subnetter.ex
@@ -9,8 +9,15 @@ defmodule Subnetter do
       binary_mask
       |> String.graphemes 
       |> Enum.count(& &1 == "1")
-    network_portion_of_ip = String.slice(binary_ip, 1..number_of_ones_in_mask)
-    IO.puts(network_portion_of_ip)
+    network_portion_of_ip = String.slice(binary_ip, 0..(number_of_ones_in_mask - 1))
+    zeroes_for_subnet_address = List.duplicate("0", 32 - number_of_ones_in_mask)
+    ones_for_broadcast_address = List.duplicate("1", 32 - number_of_ones_in_mask)
+    
+    flattened_zeroes_for_subnet_address = List.flatten(zeroes_for_subnet_address)
+    flattened_ones_for_broadcast_address = List.flatten(ones_for_broadcast_address)
+
+    subnet_address = network_portion_of_ip ++ flattened_zeroes_for_subnet_address
+    broadcast_address = network_portion_of_ip ++ flattened_ones_for_broadcast_address
   end
 
   def dotted_decimal_to_binary(dotted_decimal) do
@@ -29,7 +36,7 @@ defmodule Subnetter do
     third_octet_binary = make_eight_bits_long(raw_third_octet_binary)
     fourth_octet_binary = make_eight_bits_long(raw_fourth_octet_binary)
 
-    binary_return = "#{first_octet_binary}#{second_octet_binary}#{third_octet_binary}#{fourth_octet_binary}"
+    "#{first_octet_binary}#{second_octet_binary}#{third_octet_binary}#{fourth_octet_binary}"
   end
 
   def make_eight_bits_long(binary) do
