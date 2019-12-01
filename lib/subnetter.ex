b4e8c0cde0b415ad21744094d3f8318e9f17069a
roostercrab <jesse.shea@qixingdesign.com>
2019-12-01 11:24:25 -0800

    WORKING

--- lib/subnetter.ex
+++ lib/subnetter.ex
@@ -10,38 +10,22 @@ defmodule Subnetter do
     parsed_address = InetCidr.parse_address!(string_ip_address)
     {first_octet, second_octet, third_octet, fourth_octet} = parsed_address
 
-    IO.puts(first_octet)
-    IO.puts(second_octet)
-    IO.puts(third_octet)
-    IO.puts(fourth_octet)
-
     raw_first_octet_binary = Integer.to_string(first_octet, 2)
-    IO.puts(raw_first_octet_binary)
-
     raw_second_octet_binary = Integer.to_string(second_octet, 2)
-    IO.puts(raw_second_octet_binary)
-
     raw_third_octet_binary = Integer.to_string(third_octet, 2)
-    IO.puts(raw_third_octet_binary)
-
     raw_fourth_octet_binary = Integer.to_string(fourth_octet, 2)
-    IO.puts(raw_fourth_octet_binary)
-
-    make_eight_bits_long(raw_first_octet_binary)
-    make_eight_bits_long(raw_second_octet_binary)
-    make_eight_bits_long(raw_third_octet_binary)
-    make_eight_bits_long(raw_fourth_octet_binary)
+    
+    first_octet_binary = make_eight_bits_long(raw_first_octet_binary)
+    second_octet_binary = make_eight_bits_long(raw_second_octet_binary)
+    third_octet_binary = make_eight_bits_long(raw_third_octet_binary)
+    fourth_octet_binary = make_eight_bits_long(raw_fourth_octet_binary)
   end
 
   def make_eight_bits_long(binary) do
-    case String.length(binary) do
-      8 ->
-        IO.puts("good binary")
-      _ ->
-        number_of_bits = String.length(binary)
-        IO.puts(number_of_bits)
-    end
+    number_of_bits = String.length(binary)
+    needed_zeroes = 8 - number_of_bits
+    zeroes = List.duplicate("0", needed_zeroes)
+    eight_bits = zeroes ++ binary
+    IO.puts(eight_bits)
   end
-
-  
 end
