b1e4317ac3d53ef8d780be975bdad6d97f2804a0
roostercrab <jesse.shea@qixingdesign.com>
2019-12-01 12:47:46 -0800

    making binary

--- lib/subnetter.ex
+++ lib/subnetter.ex
@@ -2,12 +2,17 @@ defmodule Subnetter do
   @moduledoc """
   Documentation for Subnetter.
   """
-  def get_address_and_mask do
-    ip_address = IO.gets("What is the IP address? ")
-    # subnet_mask = IO.gets("What is the subnet mask? ")
-    string_ip_address_with_new_line = to_string(ip_address)
-    string_ip_address = String.trim(string_ip_address_with_new_line, "\n")
-    parsed_address = InetCidr.parse_address!(string_ip_address)
+  def get_address_and_mask(ip_address, subnet_mask) do
+    binary_ip = dotted_decimal_to_binary(ip_address)
+    IO.puts(binary_ip)
+    binary_mask = dotted_decimal_to_binary(subnet_mask)
+    IO.puts(binary_mask)
+  end
+
+  def dotted_decimal_to_binary(dotted_decimal) do
+    string_dotted_decimal_with_new_line = to_string(dotted_decimal)
+    string_dotted_decimal = String.trim(string_dotted_decimal_with_new_line, "\n")
+    parsed_address = InetCidr.parse_address!(string_dotted_decimal)
     {first_octet, second_octet, third_octet, fourth_octet} = parsed_address
 
     raw_first_octet_binary = Integer.to_string(first_octet, 2)
@@ -19,6 +24,10 @@ defmodule Subnetter do
     second_octet_binary = make_eight_bits_long(raw_second_octet_binary)
     third_octet_binary = make_eight_bits_long(raw_third_octet_binary)
     fourth_octet_binary = make_eight_bits_long(raw_fourth_octet_binary)
+
+    IO.puts("#{first_octet_binary}, #{second_octet_binary}, #{third_octet_binary}, #{fourth_octet_binary}")
+
+    binary_return = "#{first_octet_binary}#{second_octet_binary}#{third_octet_binary}#{fourth_octet_binary}"
   end
 
   def make_eight_bits_long(binary) do
@@ -26,6 +35,6 @@ defmodule Subnetter do
     needed_zeroes = 8 - number_of_bits
     zeroes = List.duplicate("0", needed_zeroes)
     eight_bits = zeroes ++ binary
-    IO.puts(eight_bits)
+    
   end
 end
