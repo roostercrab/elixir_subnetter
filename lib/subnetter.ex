43216fb269ef04e55a842326297b1618f6574e46
roostercrab <jesse.shea@qixingdesign.com>
2019-12-01 09:28:06 -0800

    getting somewhere...

--- lib/subnetter.ex
+++ lib/subnetter.ex
@@ -3,9 +3,12 @@ defmodule Subnetter do
   Documentation for Subnetter.
   """
   def get_address_and_mask do
-    ip_address = IO.gets("What is the IP address?")
-    subnet_mask = IO.gets("What is the subnet mask?")
-    IO.puts(ip_address)
-    IO.puts(subnet_mask)
+    ip_address = IO.gets("What is the IP address? ")
+    # subnet_mask = IO.gets("What is the subnet mask? ")
+    string_ip_address_with_new_line = to_string(ip_address)
+    string_ip_address = String.trim(string_ip_address_with_new_line, "\n")
+    parsed_address = InetCidr.parse_address!(string_ip_address)
+
+    {first, second, third, fourth} = parsed_address
   end
 end
