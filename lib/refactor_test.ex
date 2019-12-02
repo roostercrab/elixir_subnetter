414152653bdb42974ef0a49862bf2153a23c309d
roostercrab <jesse.shea@qixingdesign.com>
2019-12-02 10:04:14 -0800

    that didn't work

--- lib/refactor_test.ex
+++ lib/refactor_test.ex
@@ -4,7 +4,8 @@ defmodule SubnetterRefactor do
   """
   def main(ip_address, subnet_mask) do
     break_up_dotted_decimal(ip_address, subnet_mask)
-    # |> convert_decimal_to_binary
+    |> convert_decimal_to_binary
+
     # |> measure_network_portion
     # |> calculate_subnet_range
     # |> convert_binary_to_dotted_decimal
@@ -13,19 +14,24 @@ defmodule SubnetterRefactor do
   def break_up_dotted_decimal(ip_address, subnet_mask) do
     ip_address_list = String.split(ip_address, ".")
     [ip_octet_1, ip_octet_2, ip_octet_3, ip_octet_4] = ip_address_list
-    IO.puts(ip_octet_1)
-    IO.puts(ip_octet_2)
-    IO.puts(ip_octet_3)
-    IO.puts(ip_octet_4)
 
     subnet_mask_list = String.split(subnet_mask, ".")
     [mask_octet_1, mask_octet_2, mask_octet_3, mask_octet_4] = subnet_mask_list
-    IO.puts(mask_octet_1)
-    IO.puts(mask_octet_2)
-    IO.puts(mask_octet_3)
-    IO.puts(mask_octet_4)
 
-    IO.puts([ip_address_list, subnet_mask_list])
-    [ip_address_list, subnet_mask_list]
+    IO.puts(ip_address_list ++ subnet_mask_list)
+    ip_address_list ++ subnet_mask_list
+  end
+
+  def convert_decimal_to_binary(combined_list) do
+    [bin_ip_octet_1, bin_ip_octet_2, bin_ip_octet_3, bin_ip_octet_4, bin_mask_octet_1, bin_mask_octet_2, bin_mask_octet_3, bin_mask_octet_4] = combined_list
+
+    IO.inspect(bin_ip_octet_1)
+    IO.inspect(bin_ip_octet_2)
+    IO.inspect(bin_ip_octet_3)
+    IO.inspect(bin_ip_octet_4)
+    IO.inspect(bin_mask_octet_1)
+    IO.inspect(bin_mask_octet_2)
+    IO.inspect(bin_mask_octet_3)
+    IO.inspect(bin_mask_octet_4)
   end
 end
