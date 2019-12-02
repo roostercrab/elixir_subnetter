89518bcef56a9b7c98fd41f63c123306f35321c9
roostercrab <jesse.shea@qixingdesign.com>
2019-12-02 09:35:02 -0800

    WORKING need to change each to binary

--- lib/refactor_test.ex
+++ lib/refactor_test.ex
@@ -9,12 +9,23 @@ defmodule SubnetterRefactor do
     # |> calculate_subnet_range
     # |> convert_binary_to_dotted_decimal
   end
-  
+
   def break_up_dotted_decimal(ip_address, subnet_mask) do
-      {ip_octet_1, ip_octet_2, ip_octet_3, ip_octet_4} = ip_address
-      IO.puts(ip_octet_1)
-      IO.puts(ip_octet_2)
-      IO.puts(ip_octet_3)
-      IO.puts(ip_octet_4)
+    ip_address_list = String.split(ip_address, ".")
+    [ip_octet_1, ip_octet_2, ip_octet_3, ip_octet_4] = ip_address_list
+    IO.puts(ip_octet_1)
+    IO.puts(ip_octet_2)
+    IO.puts(ip_octet_3)
+    IO.puts(ip_octet_4)
+
+    subnet_mask_list = String.split(subnet_mask, ".")
+    [mask_octet_1, mask_octet_2, mask_octet_3, mask_octet_4] = subnet_mask_list
+    IO.puts(mask_octet_1)
+    IO.puts(mask_octet_2)
+    IO.puts(mask_octet_3)
+    IO.puts(mask_octet_4)
+
+    IO.puts([ip_address_list, subnet_mask_list])
+    [ip_address_list, subnet_mask_list]
   end
 end
