36c08f7a9584d428684ff22822409593b7350e88
roostercrab <jesse.shea@qixingdesign.com>
2019-12-02 10:23:58 -0800

    WORKING -> fill binary out to 8 bits

--- lib/refactor_test.ex
+++ lib/refactor_test.ex
@@ -5,7 +5,7 @@ defmodule SubnetterRefactor do
   def main(ip_address, subnet_mask) do
     break_up_dotted_decimal(ip_address, subnet_mask)
     |> convert_decimal_to_binary
-
+    
     # |> measure_network_portion
     # |> calculate_subnet_range
     # |> convert_binary_to_dotted_decimal
@@ -18,20 +18,20 @@ defmodule SubnetterRefactor do
     subnet_mask_list = String.split(subnet_mask, ".")
     [mask_octet_1, mask_octet_2, mask_octet_3, mask_octet_4] = subnet_mask_list
 
-    IO.puts(ip_address_list ++ subnet_mask_list)
     ip_address_list ++ subnet_mask_list
   end
 
   def convert_decimal_to_binary(combined_list) do
-    [bin_ip_octet_1, bin_ip_octet_2, bin_ip_octet_3, bin_ip_octet_4, bin_mask_octet_1, bin_mask_octet_2, bin_mask_octet_3, bin_mask_octet_4] = combined_list
+    integer_list =
+      for octet <- combined_list do
+        String.to_integer(octet, 10)
+      end
 
-    IO.inspect(bin_ip_octet_1)
-    IO.inspect(bin_ip_octet_2)
-    IO.inspect(bin_ip_octet_3)
-    IO.inspect(bin_ip_octet_4)
-    IO.inspect(bin_mask_octet_1)
-    IO.inspect(bin_mask_octet_2)
-    IO.inspect(bin_mask_octet_3)
-    IO.inspect(bin_mask_octet_4)
+    bin_list =
+      for octet <- integer_list do
+        Integer.to_string(octet, 2)
+      end
+
+    IO.inspect(bin_list)
   end
 end
